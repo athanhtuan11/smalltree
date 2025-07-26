d7894abb5a1ca89a46cb50c03f93144b8ac4f50e
Hoang Thanh Tuan (MS/EDS11-XC) <tuan.hoangthanh@vn.bosch.com>
2025-07-26 20:11:38 +0700

    update config file

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
+    MAX_CONTENT_LENGTH = 32 * 1024 * 1024  # 32MB, tăng giới hạn upload file
